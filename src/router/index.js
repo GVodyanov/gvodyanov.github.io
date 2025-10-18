469419d249640e660b1d92b6e7d528a68ecc0338
Grigory Vodyanov <scratchx@gmx.com>
2025-10-18 15:17:46 +0200

    fixup! feat: portfolio

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PortfolioView from '../views/PortfolioView.vue'
 const router = createRouter({
